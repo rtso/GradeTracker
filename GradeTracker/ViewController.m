369ed3f951e0b3ede7d318e0474c590a44b75d57
rtso <rtso489@gmail.com>
2015-12-31 00:06:44 -0800

    Added courses for the course picker

--- GradeTracker/ViewController.m
+++ GradeTracker/ViewController.m
@@ -8,15 +8,38 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIPickerViewDelegate>
+@property (weak, nonatomic) IBOutlet UIPickerView *CoursePicker;
+@property (weak, nonatomic) IBOutlet UIPickerView *GradeTypePicker;
+@property (weak, nonatomic) IBOutlet UITextField *myGrade;
+@property (weak, nonatomic) IBOutlet UIStackView *totalPossible;
+@property (strong, nonatomic) NSArray *courses;
 
 @end
 
 @implementation ViewController
 
+- (IBAction)doneEnteringGrade:(UIButton *)sender {
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    if (self.courses != nil) {
+        return [self.courses count];
+    }
+    return 0;
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    if (self.courses != nil) {
+        return [self.courses objectAtIndex:row];
+    }
+    return nil;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.courses = @[@"Algorithms and Models of Computation", @"Introduction to Modern Dance", @"Systems Programming", @"The World Through Museums", @"Quantum Physics"];
 }
 
 - (void)didReceiveMemoryWarning {
